e6515e2a1be628fd4ee58f85e150ba8791683cf2
Rodney Tressler <rodneytressler@Rodneys-MacBook-Pro.local>
2018-04-18 10:45:27 -0400

    [DIRECTORY STRUCTURE] packages have been created.

---
+++ app/src/main/java/com/elkcreek/rodneytressler/intermediateandroid/common/WeatherApplication.java
@@ -0,0 +1,8 @@
+package com.elkcreek.rodneytressler.intermediateandroid.common;
+
+/**
+ * Created by rodneytressler on 4/18/18.
+ */
+
+public class WeatherApplication {
+}
--- app/src/main/java/com/elkcreek/rodneytressler/intermediateandroid/ui/MainView/MainActivity.java
+++ app/src/main/java/com/elkcreek/rodneytressler/intermediateandroid/ui/MainView/MainActivity.java
@@ -1,7 +1,9 @@
-package com.elkcreek.rodneytressler.intermediateandroid;
+package com.elkcreek.rodneytressler.intermediateandroid.ui.MainView;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.elkcreek.rodneytressler.intermediateandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
