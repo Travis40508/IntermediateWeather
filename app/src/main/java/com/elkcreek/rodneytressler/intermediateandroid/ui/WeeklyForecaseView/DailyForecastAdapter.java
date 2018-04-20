81d94c70dfd11e14fac23e7c38e7c6264f23b39f
Rodney Tressler <rodneytressler@Rodneys-MacBook-Pro.local>
2018-04-20 16:20:43 -0400

    [BACKGROUND COLORS] are being properly set.

--- app/src/main/java/com/elkcreek/rodneytressler/intermediateandroid/ui/WeeklyForecaseView/DailyForecastAdapter.java
+++ app/src/main/java/com/elkcreek/rodneytressler/intermediateandroid/ui/WeeklyForecaseView/DailyForecastAdapter.java
@@ -1,5 +1,7 @@
 package com.elkcreek.rodneytressler.intermediateandroid.ui.WeeklyForecaseView;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,18 +65,36 @@ public class DailyForecastAdapter extends RecyclerView.Adapter<DailyForecastAdap
         @BindView(R.id.text_item_precip_chance)
         protected TextView dayPrecipChance;
 
+        @BindView(R.id.item_background)
+        protected ConstraintLayout layoutBackground;
+
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
 
         public void bindDays(DarkSkyApi.Days days) {
+            layoutBackground.setBackgroundColor(getBackgroundColor(days.getHighTemp()));
             dayDate.setText(days.getDate());
             dayEmoji.setText(showIcon(days.getIcon()));
             daySummary.setText(days.getSummary());
             dayHigh.setText(days.highTempString());
             dayLow.setText(days.lowTempString());
             dayPrecipChance.setText(days.precipChanceString());
+        }
+
+        private int getBackgroundColor(double highTemp) {
+            if(highTemp >= 100) {
+                return Color.parseColor("#FFABAB");
+            } else if (highTemp >= 75) {
+                return Color.parseColor("#FF8E8C");
+            } else if (highTemp >= 50) {
+                return Color.parseColor("#8589FF");
+            } else if (highTemp >= 25) {
+                return Color.parseColor("#AFCBFF");
+            } else {
+                return Color.parseColor("#6EB5FF");
+            }
         }
 
         private String showIcon(String icon) {
